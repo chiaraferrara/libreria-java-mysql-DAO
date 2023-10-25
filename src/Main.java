00966294d9f1463223a4bb4fdf3c01994be7017f
choorywey <123738595+choorywey@users.noreply.github.com>
2023-10-25 16:35:28 +0200

    switch case

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,44 @@ import dao.ConcreteLibroDAO;
 import datasource.ConnectionDBH;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     private static datasource.ConnectionDBH ConnectionDBH;
 
     public static void main(String[] args) throws IOException {
+        boolean exit = false;
+        int scelta;
         ConnectionDBH connectionDBH = new ConnectionDBH();
         ConcreteLibroDAO concreteLibroDAO = new ConcreteLibroDAO(ConnectionDBH);
-        concreteLibroDAO.readLibri();
-        try {
-            concreteLibroDAO.add();
-        }catch (IOException e){
-            System.out.println(e);
-        }
+        while (exit == false) {
+            System.out.print("-----------------------------" +
+                    "\nCosa vorresti fare?\n" +
+                    "1) Aggiungi libro\n" +
+                    "2) Visualizza Libri\n" +
+                    "3) Modifica Libro\n" +
+                    "4) Rimuovi Libro" +
+                    "5) Esci \n\n" +
+                    "Inserisci il numero della tua scelta.\n" +
+                    "-----------------------------\n");
+            Scanner s = new Scanner(System.in);
+            scelta = s.nextInt();
+            switch (scelta){
+                case 1:
 
+                    concreteLibroDAO.add();
+                    break;
+                case 2:
+                    concreteLibroDAO.readLibri();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+            }
+        }
     }
 }
